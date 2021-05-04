9b53f279e7bfa53d668698cad25ad1663c14f788
Natanael Vieira <natanael.vieira.barboza@gmail.com>
2021-05-04 19:57:38 -0300

    chore(Routes): adicionando rota para "tasks"

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,7 +1,11 @@
 import { Router } from "express";
+import { usersRoutes } from "./user.routes";
+import { tasksRouter } from "./tasks.routes";
 
 const router = Router();
-router.use("/users");
+
+router.use("/users", usersRoutes);
+router.use("/tasks", tasksRouter);
 
 export { router };
 
