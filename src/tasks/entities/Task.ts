fa9643f6d0d393afd450f93e85d4eadb8a8b3967
Natanael Vieira <natanael.vieira.barboza@gmail.com>
2021-05-04 19:59:40 -0300

    chore(Task): implementando entidade Task com seus respectivos atributos

---
+++ src/tasks/entities/Task.ts
@@ -0,0 +1,21 @@
+import { v4 as uuidV4 } from "uuid";
+
+
+class Task {
+
+    id: string;
+    title: string;
+    description: string;
+    done: boolean;
+    created_at: Date;
+    updated_at: Date;
+
+    constructor() {
+        if (!this.id) {
+            this.id = uuidV4();
+            this.done = false;
+        }
+    }
+}
+
+export { Task };
