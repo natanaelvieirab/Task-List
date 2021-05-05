5b341b5d19f03d2bd7e846ac563e387d5f49fab5
Natanael Vieira <natanael.vieira.barboza@gmail.com>
2021-05-04 23:19:28 -0300

    refactor(User/UseCase): alterado classe createUserUseCase para retornar o usuário criado

--- src/users/useCase/createUser/CreateUserUseCase.ts
+++ src/users/useCase/createUser/CreateUserUseCase.ts
@@ -1,22 +1,25 @@
 import { AppError } from "../../../utils/AppError"
 import { IUserDTO } from "../../dtos/IUserDTO";
+import { User } from "../../entities/User";
 import { IUserRepository } from "../../repositories/IUserRepository";
 
 class CreateUserUseCase {
     constructor(private userRepository: IUserRepository) { }
 
-    async execute({ name, email, password }: IUserDTO) {
+    async execute({ name, email, password }: IUserDTO): Promise<User> {
         const user = this.userRepository.findByEmail(email);
 
         if (user) {
             throw new AppError("User already exists!");
         }
 
-        this.userRepository.createUser({
+        const newUser = this.userRepository.createUser({
             name,
             email,
             password
         });
+
+        return newUser;
     }
 }
 
