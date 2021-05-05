3655ec220e06a386bb7f5f88c86b66ac9f5e35fc
Natanael Vieira <natanael.vieira.barboza@gmail.com>
2021-05-04 23:17:41 -0300

    refactor(Repository/ User): alterando na interface de repositorio o tipo de retorno
    O metodo ira retornar o "User"

--- src/users/repositories/IUserRepository.ts
+++ src/users/repositories/IUserRepository.ts
@@ -2,10 +2,10 @@ import { IUserDTO } from "../dtos/IUserDTO";
 import { User } from "../entities/User";
 
 
-
 interface IUserRepository {
-    createUser(data: IUserDTO): Promise<void>;
+    createUser(data: IUserDTO): Promise<User>;
     findByEmail(email: string): Promise<User | undefined>;
+    findById(id: string): Promise<User>;
 }
 
 export { IUserRepository };
