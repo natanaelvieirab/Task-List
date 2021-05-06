da90efb45e16dde4e0dcccb1d6ccc7aa21854146
Natanael Vieira <natanael.vieira.barboza@gmail.com>
2021-05-05 21:06:08 -0300

    chore(Tsyringe): importando "reflect-metadata" e o container contendo os singleton

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,5 @@
+import 'reflect-metadata';
+import './shared/container/';
 import 'express-async-errors';
 import express, { NextFunction, Request, Response } from "express";
 import { router } from "./routes";
