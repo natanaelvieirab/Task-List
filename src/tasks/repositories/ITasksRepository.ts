fa78f136fc1dd892b02cdd0e59b51b4597980711
Natanael Vieira <natanael.vieira.barboza@gmail.com>
2021-05-05 22:03:26 -0300

    feat(Repository/ Task): definindo a assinatura do metodo "showAllTasks"

--- src/tasks/repositories/ITasksRepository.ts
+++ src/tasks/repositories/ITasksRepository.ts
@@ -5,6 +5,8 @@ import { Task } from "../entities/Task";
 
 interface ITasksRepository {
     createTask(data: ITaskDTO): Promise<Task>;
+    showAllTasks(): Promise<Task[]>
+
 }
 
 export { ITasksRepository };
