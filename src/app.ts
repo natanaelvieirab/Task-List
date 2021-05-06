6443ae1c28a66440b7381f5747dd5da9ff52e764
Natanael Vieira <natanael.vieira.barboza@gmail.com>
2021-05-05 21:02:47 -0300

    fix(App): corrigindo "ERROR: UnhandledPromiseRejectionWarning"
    Para corrigir esse erro, foi implementando no arquivo app um tratamento de dos errors emitidos pelo 'throw', e ainda foi adicionada o import "express-async-errors" para que o express possa entender a função do 'throw'

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,25 @@
-import express from "express";
+import 'express-async-errors';
+import express, { NextFunction, Request, Response } from "express";
 import { router } from "./routes";
+import { AppError } from "./shared/errors/AppError";
+
 
 var app = express();
 
 app.use(router);
+
+app.use((err: Error, request: Request, response: Response, next: NextFunction) => {
+
+    if (err instanceof AppError) {
+        return response.status(err.statusCode).json({
+            message: err.message
+        });
+    }
+
+    return response.status(500).json({
+        status: 'error',
+        message: `Internal server error - ${err.message}`
+    });
+});
 
 export { app };
