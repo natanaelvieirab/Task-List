c8c7ec1c4a1c96325b893e533758f7f342ca6242
Natanael Vieira <natanael.vieira.barboza@gmail.com>
2021-05-05 22:02:26 -0300

    feat(Routes): implementando rota para exibir todos as "tasks"

--- src/routes/tasks.routes.ts
+++ src/routes/tasks.routes.ts
@@ -1,10 +1,14 @@
 import { Router } from "express";
 import { CreateTaskController } from "../tasks/useCase/createTask/CreateTaskController";
+import { ShowAllTasksController } from "../tasks/useCase/showAllTasks/ShowAllTasksController"
 
 const tasksRouter = Router();
 
 const createTasksController = new CreateTaskController();
+const showAllTasksController = new ShowAllTasksController();
 
 tasksRouter.post("/", createTasksController.handle);
+tasksRouter.get("/", showAllTasksController.handle)
+
 
 export { tasksRouter };
