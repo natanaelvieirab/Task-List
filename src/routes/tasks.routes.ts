88d2e5295115e9f5807fb0a215f2ba76f0f389c9
Natanael Vieira <natanael.vieira.barboza@gmail.com>
2021-05-05 22:18:56 -0300

    refactor(Routes):

--- src/routes/tasks.routes.ts
+++ src/routes/tasks.routes.ts
@@ -1,14 +1,17 @@
 import { Router } from "express";
 import { CreateTaskController } from "../tasks/useCase/createTask/CreateTaskController";
 import { ShowAllTasksController } from "../tasks/useCase/showAllTasks/ShowAllTasksController"
+import { ShowOneTaskController } from "../tasks/useCase/showOneTask/ShowOneTaskController";
 
 const tasksRouter = Router();
 
 const createTasksController = new CreateTaskController();
 const showAllTasksController = new ShowAllTasksController();
+const showOneTaskController = new ShowOneTaskController();
 
 tasksRouter.post("/", createTasksController.handle);
 tasksRouter.get("/", showAllTasksController.handle)
+tasksRouter.get("/:id", showOneTaskController.handle)
 
 
 export { tasksRouter };
