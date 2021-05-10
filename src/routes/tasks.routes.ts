320d2febd23264388e66d328e766807dd2050a68
Natanael Vieira <natanael.vieira.barboza@gmail.com>
2021-05-10 10:11:05 -0300

    chore(Routes): adicionando rota para atualizar a situação de uma task

--- src/routes/tasks.routes.ts
+++ src/routes/tasks.routes.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import { CreateTaskController } from "../tasks/useCase/createTask/CreateTaskController";
 import { ShowAllTasksController } from "../tasks/useCase/showAllTasks/ShowAllTasksController"
 import { ShowOneTaskController } from "../tasks/useCase/showOneTask/ShowOneTaskController";
+import { UpdateDoneController } from "../tasks/useCase/updateDone/UpdateDoneController";
 import { UpdateTaskController } from "../tasks/useCase/updateTask/UpdateTaskController";
 
 const tasksRouter = Router();
@@ -10,11 +11,12 @@ const createTasksController = new CreateTaskController();
 const showAllTasksController = new ShowAllTasksController();
 const showOneTaskController = new ShowOneTaskController();
 const updateTaskUseCase = new UpdateTaskController();
+const updateDoneController = new UpdateDoneController();
 
 tasksRouter.post("/", createTasksController.handle);
 tasksRouter.get("/", showAllTasksController.handle)
 tasksRouter.get("/:id", showOneTaskController.handle)
 tasksRouter.put("/:id", updateTaskUseCase.handle)
-
+tasksRouter.patch("/:id/done", updateDoneController.handle);
 
 export { tasksRouter };
