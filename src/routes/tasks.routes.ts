76ee243c4ab4cc13a557a1d4228a2b2272d1faa0
Natanael Vieira <natanael.vieira.barboza@gmail.com>
2021-05-10 09:48:45 -0300

    chore(Routes): criando rota para atualizar uma task

--- src/routes/tasks.routes.ts
+++ src/routes/tasks.routes.ts
@@ -2,16 +2,19 @@ import { Router } from "express";
 import { CreateTaskController } from "../tasks/useCase/createTask/CreateTaskController";
 import { ShowAllTasksController } from "../tasks/useCase/showAllTasks/ShowAllTasksController"
 import { ShowOneTaskController } from "../tasks/useCase/showOneTask/ShowOneTaskController";
+import { UpdateTaskController } from "../tasks/useCase/updateTask/UpdateTaskController";
 
 const tasksRouter = Router();
 
 const createTasksController = new CreateTaskController();
 const showAllTasksController = new ShowAllTasksController();
 const showOneTaskController = new ShowOneTaskController();
+const updateTaskUseCase = new UpdateTaskController();
 
 tasksRouter.post("/", createTasksController.handle);
 tasksRouter.get("/", showAllTasksController.handle)
 tasksRouter.get("/:id", showOneTaskController.handle)
+tasksRouter.put("/:id", updateTaskUseCase.handle)
 
 
 export { tasksRouter };
