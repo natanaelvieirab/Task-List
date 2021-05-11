29d5e72e424ad7728adc9ea04aa8dceafb2a50c6
Natanael Vieira <natanael.vieira.barboza@gmail.com>
2021-05-11 20:13:16 -0300

    chore(User/Controller): implementando controller para atualizar user

---
+++ src/users/useCase/updateUser/UpdateUserController.ts
@@ -0,0 +1,21 @@
+import { Request, Response } from "express";
+import { container } from "tsyringe";
+import { UpdateUserUseCase } from "./UpdateUserUseCase";
+
+
+class UpdateUserController {
+
+    async handle(request: Request, response: Response): Promise<Response> {
+
+        const { id } = request.params;
+        const { name, password } = request.body;
+
+        const updateUserUseCase = container.resolve(UpdateUserUseCase);
+
+        const user = await updateUserUseCase.execute({ id, name, password });
+
+        return response.status(200).json(user);
+    }
+}
+
+export { UpdateUserController };
