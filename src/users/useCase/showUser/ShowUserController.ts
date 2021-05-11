2a7db4fec51a8d2b16c58276b04a13136c9127ad
Natanael Vieira <natanael.vieira.barboza@gmail.com>
2021-05-11 20:14:13 -0300

    chore(User/Controller): implementando controller para exibir os dados de um user

---
+++ src/users/useCase/showUser/ShowUserController.ts
@@ -0,0 +1,22 @@
+import { Request, Response } from "express";
+import { container } from "tsyringe";
+import { ShowUserUseCase } from "./ShowUserUseCase";
+
+
+
+
+class ShowUserController {
+
+    async handle(request: Request, response: Response): Promise<Response> {
+
+        const { id } = request.params;
+
+        const showUserUseCase = container.resolve(ShowUserUseCase);
+
+        const user = await showUserUseCase.execute(id);
+
+        return response.status(200).json(user);
+    }
+}
+
+export { ShowUserController };
