83babfb276cb1bcac8c07483cf19e984d5934b22
Natanael Vieira <natanael.vieira.barboza@gmail.com>
2021-05-11 22:18:03 -0300

    chore(User/Routes): adicionando rotas para atualizar, buscar e deletar um user

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,12 +1,22 @@
 import { Router } from "express";
 import { CreateUserController } from "../users/useCase/createUser/CreateUserController";
+import { DeleteUserController } from "../users/useCase/deleteUser/DeleteUserController";
+import { ShowUserController } from "../users/useCase/showUser/ShowUserController";
+import { UpdateUserController } from "../users/useCase/updateUser/UpdateUserController";
 
 
 var usersRoutes = Router();
 
 const createUserController = new CreateUserController();
+const updateUserController = new UpdateUserController();
+const showUserController = new ShowUserController();
+const deleteUserController = new DeleteUserController();
+
 
 usersRoutes.post("/", createUserController.handle);
+usersRoutes.put("/:id", updateUserController.handle);
+usersRoutes.get("/:id", showUserController.handle);
+usersRoutes.delete("/:id", deleteUserController.handle);
 
 export { usersRoutes };
 
