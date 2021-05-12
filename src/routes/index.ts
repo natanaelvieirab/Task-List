50ca4c25b922dc2c4a5b1d96430086709fb5db90
Natanael Vieira <natanael.vieira.barboza@gmail.com>
2021-05-11 21:58:15 -0300

    chore(Routes): adicionando a rota authenticateRoutes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,11 +1,13 @@
 import { Router } from "express";
 import { usersRoutes } from "./user.routes";
 import { tasksRouter } from "./tasks.routes";
+import { authenticateRoutes } from "./authenticate.routes";
 
 const router = Router();
 
 router.use("/users", usersRoutes);
 router.use("/tasks", tasksRouter);
+router.use(authenticateRoutes);
 
 export { router };
 
