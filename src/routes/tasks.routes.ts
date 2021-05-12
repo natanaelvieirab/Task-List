2b92bc75bfcfc369003bc96f701fe1515c502b06
Natanael Vieira <natanael.vieira.barboza@gmail.com>
2021-05-11 22:19:45 -0300

    chore(Routes): adicionando rota para deletar uma task

--- src/routes/tasks.routes.ts
+++ src/routes/tasks.routes.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import { CreateTaskController } from "../tasks/useCase/createTask/CreateTaskController";
+import { DeleteTaskController } from "../tasks/useCase/deleteTaskController/DeleteTaskController";
 import { ShowAllTasksController } from "../tasks/useCase/showAllTasks/ShowAllTasksController"
 import { ShowOneTaskController } from "../tasks/useCase/showOneTask/ShowOneTaskController";
 import { UpdateDoneController } from "../tasks/useCase/updateDone/UpdateDoneController";
@@ -12,11 +13,15 @@ const showAllTasksController = new ShowAllTasksController();
 const showOneTaskController = new ShowOneTaskController();
 const updateTaskController = new UpdateTaskController();
 const updateDoneController = new UpdateDoneController();
+const deleteTaskController = new DeleteTaskController();
+
 
 tasksRouter.post("/", createTasksController.handle);
 tasksRouter.get("/", showAllTasksController.handle)
 tasksRouter.get("/:id", showOneTaskController.handle)
 tasksRouter.put("/:id", updateTaskController.handle)
 tasksRouter.patch("/:id/done", updateDoneController.handle);
+tasksRouter.delete("/:id", deleteTaskController.handle);
+
 
 export { tasksRouter };
