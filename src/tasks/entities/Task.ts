ee9d3e34a7c5237419be7de3f5d49aabdebb7bf7
Natanael Vieira <natanael.vieira.barboza@gmail.com>
2021-07-12 20:28:33 -0300

    refactor(Tasks): adicionado notação

--- src/tasks/entities/Task.ts
+++ src/tasks/entities/Task.ts
@@ -1,14 +1,33 @@
+import { Column, CreateDateColumn, Entity, JoinColumn, ManyToOne, PrimaryColumn } from "typeorm";
 import { v4 as uuidV4 } from "uuid";
+import { User } from "../../users/entities/User";
 
-
+@Entity("tasks")
 class Task {
 
+    @PrimaryColumn()
     id: string;
+
+    @Column()
     title: string;
+
+    @Column()
     description: string;
+
+    @Column()
     done: boolean;
+
+    @Column()
     id_user: string;
+
+    @ManyToOne(() => User)
+    @JoinColumn({ name: 'id_user' })
+    user: User;
+
+    @CreateDateColumn()
     created_at: Date;
+
+    @CreateDateColumn()
     updated_at: Date;
 
     constructor() {
